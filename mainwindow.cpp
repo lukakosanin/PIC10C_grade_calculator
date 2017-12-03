940a1c523e823ebeafed21b0d15480efeb48ae65
Luka Kosanin <avivahirsch@Avivas-MacBook-Pro.local>
2017-12-03 12:03:57 -0800

    Connecting the buttons and variables

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,60 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QString>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow), HW1(0),HW2(0), HW3(0), HW4(0), HW5(0), HW6(0), HW7(0), HW8(0), Mid1(0), Mid2(0), Final_score(0),
     optA(true), optB(false), optC(false)
 {
     ui->setupUi(this);
+
+    /*connect(ui->hw1_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW1)));
+    connect(ui->hw2_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW2)));
+    connect(ui->hw3_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW3)));
+    connect(ui->hw4_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW4)));
+    connect(ui->hw5_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW5)));
+    connect(ui->hw6_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW6)));
+    connect(ui->hw7_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW7)));
+    connect(ui->hw8_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(HW8)));
+    connect(ui->Mid1_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(Mid1)));
+    connect(ui->Mid2_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(Mid2)));
+    connect(ui->Final_spinbox,SIGNAL(valueChanged(int)),this,SLOT(update_grade(Final_score)));
+*/
+    connect(ui->pushButton,SIGNAL(clicked(bool)),this,SLOT(update_variables()));
+    connect(ui->pushButton,SIGNAL(clicked(bool)),this,SLOT(calculate_grade()));
+
+}
+double MainWindow::grade_A() const
+{
+    double temp = 0.0;
+    temp = ((.25*((HW1+HW2+HW3+HW4+HW5+HW6+HW7+HW8)/8))+(.2*Mid1)+(.2*Mid2)+(.35*Final_score));
+    return (temp);
+
+}
+double MainWindow::grade_B() const
+{
+    double temp = 0.0;
+    if (Mid1>Mid2)
+    {
+    temp = ((.25*((HW1+HW2+HW3+HW4+HW5+HW6+HW7+HW8)/8))+(.3*Mid1)+(.45*Final_score));
+    }
+    else
+    {
+        temp = ((.25*((HW1+HW2+HW3+HW4+HW5+HW6+HW7+HW8)/8))+(.3*Mid2)+(.45*Final_score));
+    }
+    return (temp);
+}
+double MainWindow::max() const
+{
+    double a = grade_A();
+    double b = grade_B();
+    if(a>b)
+        return a;
+    else
+        return b;
+}
+void MainWindow::update_variables()
+{
     HW1 = ui->hw1_spinbox->value();
     HW2 = ui->hw2_spinbox->value();
     HW3 = ui->hw3_spinbox->value();
@@ -19,11 +67,27 @@ MainWindow::MainWindow(QWidget *parent) :
     Mid2 = ui->Mid2_spinbox->value();
     Final_score = ui->Final_spinbox->value();
     optA = ui->radioButton->isChecked();
-    optB = ui->radioButton->isChecked();
-    optC = ui->radioButton->isChecked();
-
+    optB = ui->radioButton_2->isChecked();
+    optC = ui->radioButton_3->isChecked();
 }
-
+void MainWindow::calculate_grade()
+{
+    QString grade = "";
+    if(optA == true)
+    {
+        grade = QString::number(grade_A());
+    }
+    else if (optB == true)
+    {
+        grade = QString::number(grade_B());
+    }
+    else if (optC == true)
+    {
+        grade = QString::number(max());
+    }
+    grade+="%";
+    ui->lineEdit->setText(grade);
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- mainwindow.h
+++ mainwindow.h
@@ -13,10 +13,13 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    double grade_A() const;
+    double grade_B() const;
+    double max() const;
     ~MainWindow();
 public slots:
-    //void calculate_grade();
-
+    void calculate_grade();
+    void update_variables();
 private:
     Ui::MainWindow *ui;
     int HW1, HW2, HW3, HW4, HW5, HW6, HW7, HW8, Mid1, Mid2, Final_score;
