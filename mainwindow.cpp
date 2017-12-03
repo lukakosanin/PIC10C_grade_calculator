083ba2e0fd7fb5d219dfc12c17875145aa5126e5
Luka Kosanin <avivahirsch@Avivas-MacBook-Pro.local>
2017-12-03 10:41:59 -0800

    Made connections and set up the private variables to calculate the grade

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,25 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow), HW1(0),HW2(0), HW3(0), HW4(0), HW5(0), HW6(0), HW7(0), HW8(0), Mid1(0), Mid2(0), Final_score(0),
+    optA(true), optB(false), optC(false)
 {
     ui->setupUi(this);
+    HW1 = ui->hw1_spinbox->value();
+    HW2 = ui->hw2_spinbox->value();
+    HW3 = ui->hw3_spinbox->value();
+    HW4 = ui->hw4_spinbox->value();
+    HW5 = ui->hw5_spinbox->value();
+    HW6 = ui->hw6_spinbox->value();
+    HW7 = ui->hw7_spinbox->value();
+    HW8 = ui->hw8_spinbox->value();
+    Mid1 = ui->Mid1_spinbox->value();
+    Mid2 = ui->Mid2_spinbox->value();
+    Final_score = ui->Final_spinbox->value();
+    optA = ui->radioButton->isChecked();
+    optB = ui->radioButton->isChecked();
+    optC = ui->radioButton->isChecked();
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+public slots:
+    //void calculate_grade();
 
 private:
     Ui::MainWindow *ui;
+    int HW1, HW2, HW3, HW4, HW5, HW6, HW7, HW8, Mid1, Mid2, Final_score;
+    bool optA, optB, optC;
 };
 
 #endif // MAINWINDOW_H
